2edc663fc82356d30319f766aec6eae4fea8f721
hou likai <1349042999@qq.com>
2017-09-12 10:31:11 +0800

    second

---
+++ src/main/java/SecondTest.java
@@ -0,0 +1,12 @@
+package main.java;
+
+/**
+ * @Author: houlk
+ * @Decription:
+ * @Date: Create in 10:30 2017/9/12
+ */
+public class SecondTest {
+    public static void main(String[] args) {
+        System.out.println("Second");
+    }
+}
