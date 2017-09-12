b3b9d797890eabb27c73a99760e8c2150834791a
hou likai <1349042999@qq.com>
2017-09-12 10:29:07 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package main.java;
+
+/**
+ * @Author: houlk
+ * @Decription:
+ * @Date: Create in 10:27 2017/9/12
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
